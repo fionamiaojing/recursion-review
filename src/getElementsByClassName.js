c26b2b191f1dccec217606c1b143c7a987ed8812
fionamiaojing <fionamiaojing@gmail.com>
2018-04-23 11:43:08 -0700

    finish getElementsByClassName

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -7,4 +7,21 @@
 var getElementsByClassName = function(className
 ) {
   // your code here
+  var result =[];
+  var searchNodes = function(nodes) {
+    if (nodes.classList && nodes.classList.contains(className)) {
+      result.push(nodes);
+    };
+    if (nodes.childNodes) {
+      var children = nodes.childNodes;
+      children.forEach(function(item) {
+        console.log(item)
+        searchNodes(item);
+      });
+    }
+  };
+  searchNodes(document.body);
+
+  return result;
+
 };
