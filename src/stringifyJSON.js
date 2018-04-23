5117bfaa272133d77fbbdd3362b7b297299952e1
fionamiaojing <fionamiaojing@gmail.com>
2018-04-23 15:16:22 -0700

    complete stringfyJSON

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -5,4 +5,33 @@
 
 var stringifyJSON = function(obj) {
   // your code goes here
+  if (typeof obj === 'number' || typeof obj === 'boolean') {
+    return `${obj}`;
+  } else if (obj === null) {
+    return 'null';
+  } else if(typeof obj  === 'string') {
+    return `"${obj}"`;
+  } else if(Array.isArray(obj)) {
+    var result = [];
+    for (var i = 0; i < obj.length; i++) {
+      result.push(stringifyJSON(obj[i]));    
+    }
+    return '[' + result.join(',') + ']';
+  } else if(typeof obj === 'object') {
+    var result = [];
+    for(var key in obj) {
+      if (obj[key] === undefined || typeof obj[key] === 'function') {
+        continue;
+      } else {
+        result.push(stringifyJSON(key)+':'+stringifyJSON(obj[key]));
+      }
+      
+    }
+
+    return `{${result.join(',')}}`;
+  } else if (typeof obj === 'function') {
+    return {};
+  }
+
 };
+  
